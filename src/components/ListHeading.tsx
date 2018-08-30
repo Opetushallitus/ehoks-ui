a85d8b8fe14de203bacfe7b860fac4dbf7118154
Lari Hoppula <lari.hoppula@gofore.com>
2018-08-30 16:09:43 +0300

    EH-52: ListHeading now supports icon, titleSize and onClose props.

--- src/components/ListHeading.tsx
+++ src/components/ListHeading.tsx
@@ -1,7 +1,49 @@
+import { Icon } from "components/Icon"
+import React from "react"
 import styled from "react-emotion"
+import { GoX } from "react-icons/go"
 
-export const ListHeading = styled("h1")`
+interface TitleProps {
+  titleSize?: "regular" | "large"
+}
+
+export const ListHeadingContainer = styled("h1")`
+  display: flex;
+  align-items: center;
   margin: 0;
-  font-size: 22px;
   color: #a3a3a3;
 `
+
+const Title = styled("div")`
+  font-size: ${(props: TitleProps) =>
+    props.titleSize === "large" ? "28px" : "22px"};
+  flex: 1;
+`
+
+export interface ListHeadingProps {
+  className?: string
+  icon?: React.ReactNode
+  titleSize?: "regular" | "large"
+  onClose?: () => void
+}
+
+export class ListHeading extends React.Component<ListHeadingProps> {
+  render() {
+    const {
+      children,
+      className,
+      icon,
+      titleSize = "regular",
+      onClose
+    } = this.props
+    return (
+      <ListHeadingContainer className={className}>
+        {icon}
+        <Title titleSize={titleSize}>{children}</Title>
+        <Icon>
+          {onClose ? <GoX size="24" color="#fff" onClick={onClose} /> : null}
+        </Icon>
+      </ListHeadingContainer>
+    )
+  }
+}
