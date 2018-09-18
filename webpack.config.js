70dd9e8099c640d3bb1061e0777239cd139fce9e
Lari Hoppula <lari.hoppula@gofore.com>
2018-09-18 14:51:11 +0300

    Use ts-loader & fork-ts-checker-webpack-plugin instead of awesome-typescript-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var webpack = require("webpack")
 var path = require("path")
+var ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 
 module.exports = {
   mode: "development",
+  context: __dirname,
   entry: ["./src/index.tsx"],
   output: {
     // `filename` provides a template for naming your bundles (remember to use `[name]`)
@@ -13,8 +15,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: "inline-source-map",
+
+  optimization: {},
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
@@ -22,12 +25,23 @@ module.exports = {
     modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
 
-  plugins: [],
+  plugins: [new ForkTsCheckerWebpackPlugin()],
 
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, use: "awesome-typescript-loader" },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true
+            }
+          }
+        ]
+      },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, enforce: "pre", use: "source-map-loader" },
       // Load images with 'file-loader'.
