1ea5a7c23c729661e60991c3987be75d6b8a5d92
Tuomas Kareinen <tuomas.kareinen@gofore.com>
2018-09-24 15:21:31 +0300

    build: configure app launch by env

--- scripts/ci/run.sh
+++ scripts/ci/run.sh
@@ -2,7 +2,27 @@
 
 set -euo pipefail
 
-echo "Starting Prometheus node_exporter..."
+echo "Downloading environment-specific properties…"
+aws s3 cp "s3://${ENV_BUCKET}/services/latest/opintopolku.yml" /root/oph-configuration/
+
+echo "Applying configuration templates…"
+while IFS= read -r -d '' tpl_file; do
+    target=${tpl_file%.template}
+    echo "  $tpl_file -> $target"
+    j2 "$tpl_file" /root/oph-configuration/opintopolku.yml > "$target"
+done < <(find /root -name '*.template' -print0)
+unset tpl_file target
+
+echo "Insert app boot config for this env into /root/public/index.html …"
+config_json=$(python /root/escape-html.py < /root/config.json)
+sed -f <(cat <<EOF
+s|APP-BOOT-CONFIG-DEFAULT|${config_json//&/\\&}|
+EOF
+) -i /root/public/index.html
+unset config_json
+
+echo "Starting Prometheus node_exporter…"
 nohup /root/node_exporter > /root/node_exporter.log 2>&1 &
 
+echo "Starting nginx…"
 exec nginx -g 'daemon off;'
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,27 @@
-export const apiUrl = (path: string) =>
-  `http://localhost:3000/ehoks/api/v1/${path}`
+const defaultConfig = () => ({
+  backendUrl: "http://localhost:3000/ehoks/api/v1"
+})
+
+const config = (function() {
+  const appEl = document.getElementById("app")
+
+  if (appEl == null) {
+    return defaultConfig()
+  }
+
+  const data = appEl.getAttribute("data-app-boot-config")
+
+  if (data == null || data === "APP-BOOT-CONFIG-DEFAULT") {
+    return defaultConfig()
+  }
+
+  return JSON.parse(data)
+})()
+
+if (/\bdebug=1\b/.test(window.location.search)) {
+  console.log("Config: ", config)
+}
+
+const backendUrl = config.backendUrl
+
+export const apiUrl = (path: string) => `${backendUrl}/${path}`
