c49f6e909a549a51a874b406b8b86b95203c7fa4
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2019-06-18 12:09:55 +0300

    eh-401 added script folder and files

---
+++ virkailija/scripts/ci/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export ARTIFACT_NAME=$1
+export BUILD_ID=ci-${TRAVIS_BUILD_NUMBER}
+export DOCKER_TARGET=${ECR_REPO}/${ARTIFACT_NAME}:${BUILD_ID}
+
+cp -r \
+   public \
+   scripts/ci/Dockerfile \
+   scripts/ci/config.json.template \
+   scripts/ci/escape-html.py \
+   scripts/ci/nginx-server.conf \
+   scripts/ci/run \
+   scripts/ci/run.sh \
+   "$DOCKER_BUILD_DIR/"
+
+cat > "$DOCKER_BUILD_DIR/public/ehoks/buildversion.txt" <<END
+artifactId=$ARTIFACT_NAME
+buildNumber=$BUILD_ID
+vcsRevision=$(git rev-parse HEAD)
+buildTime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+END
+
+sed "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" < scripts/ci/Dockerfile > "$DOCKER_BUILD_DIR/Dockerfile"
+
+find "$DOCKER_BUILD_DIR"
+docker build --build-arg name="$ARTIFACT_NAME" -t "$DOCKER_TARGET" "$DOCKER_BUILD_DIR"
+docker images
