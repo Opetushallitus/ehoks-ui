fce902e1a9889fa0a7348b4f42171b4e06e7f3c2
Matti Ahinko <matti.ahinko@gmail.com>
2019-06-20 14:27:18 +0300

    EH-420: Add types

--- virkailija/src/stores/KoulutuksenJarjestajaStore.ts
+++ virkailija/src/stores/KoulutuksenJarjestajaStore.ts
@@ -4,6 +4,8 @@ import min from "lodash.min"
 import { flow, getEnv, getRoot, Instance, types } from "mobx-state-tree"
 import { HOKS } from "models/HOKS"
 import { SessionUser } from "models/SessionUser"
+import { IRootStore } from "stores/RootStore"
+import { ISessionStore } from "stores/SessionStore"
 import { StoreEnvironment } from "types/StoreEnvironment"
 
 const sortKeys = ["nimi", "tutkinto", "osaamisala"] as const
@@ -93,7 +95,10 @@ const Search = types
 
     const haeOppijat = flow(function*() {
       // TODO fix cross reference of stores?
-      const oppilaitosOid = getRoot(self).session.selectedOrganisationOid
+      const rootStore: IRootStore = getRoot<IRootStore>(self)
+      const session: ISessionStore | null = rootStore ? rootStore.session : null
+      const oppilaitosOid: string =
+        rootStore && session ? session.selectedOrganisationOid : ""
       if (oppilaitosOid === "") {
         return
       }
