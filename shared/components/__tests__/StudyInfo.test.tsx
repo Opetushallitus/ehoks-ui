df1a78d10b6cc2e5080c5a875a4cb095674122ee
Lari Hoppula <lari.hoppula@gofore.com>
2019-08-30 12:25:18 +0300

    EH-371: Upgraded to React 16.9 and fixed test warnings

--- shared/components/__tests__/StudyInfo.test.tsx
+++ shared/components/__tests__/StudyInfo.test.tsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { render, fireEvent } from "@testing-library/react"
+import { render, fireEvent, wait } from "@testing-library/react"
 import { StudyInfo } from "../StudyInfo"
 import { renderWithContext } from "testUtils"
+import { Naytto, Harjoittelujakso } from "models/helpers/TutkinnonOsa"
 
 const osaamisvaatimukset = [
   {
@@ -19,6 +20,92 @@ const osaamisvaatimukset = [
   }
 ]
 
+const naytot1 = [
+  {
+    alku: "2017-10-25",
+    loppu: "2017-10-26",
+    organisaatio: "Näyttöpaikka",
+    ymparisto: "Näyttöympäristö",
+    koulutuksenJarjestajaArvioijat: [
+      "Koulu Järjestäjä, koulutuksen järjestäjä"
+    ],
+    tyoelamaArvioijat: ["Työ Arvioija, työelämä"],
+    tyotehtavat: ["Elintarvikkeiden valmistus", "Elintarvikkeiden pakkaus"],
+    tyyppi: "DEMONSTRATION" as Naytto["tyyppi"]
+  }
+]
+
+const naytot2 = [
+  {
+    alku: "2017-10-25",
+    loppu: "2017-10-26",
+    organisaatio: "Näyttöpaikka",
+    ymparisto: "Näyttöympäristö",
+    koulutuksenJarjestajaArvioijat: [
+      "Koulu Järjestäjä, koulutuksen järjestäjä"
+    ],
+    tyoelamaArvioijat: ["Työ Arvioija, työelämä"],
+    tyotehtavat: ["Elintarvikkeiden valmistus", "Elintarvikkeiden pakkaus"],
+    tyyppi: "DEMONSTRATION" as Naytto["tyyppi"]
+  },
+  {
+    alku: "2017-10-25",
+    loppu: "2017-10-26",
+    organisaatio: "Näyttöpaikka",
+    ymparisto: "Näyttöympäristö",
+    koulutuksenJarjestajaArvioijat: [
+      "Koulu Järjestäjä, koulutuksen järjestäjä"
+    ],
+    tyoelamaArvioijat: ["Työ Arvioija, työelämä"],
+    tyotehtavat: ["Elintarvikkeiden valmistus", "Elintarvikkeiden pakkaus"],
+    tyyppi: "DEMONSTRATION" as Naytto["tyyppi"]
+  }
+]
+
+const naytot3 = [
+  {
+    alku: "2019-08-01",
+    loppu: "2019-08-01",
+    organisaatio: "Näyttöpaikka",
+    ymparisto: "Näyttöympäristö",
+    koulutuksenJarjestajaArvioijat: [
+      "Olli Opettaja, koulutuksen järjestäjä",
+      "Oona Opettaja, koulutuksen järjestäjä"
+    ],
+    tyoelamaArvioijat: ["Teuvo Työpaikka, työelämä"],
+    tyotehtavat: ["Elintarvikkeiden valmistus", "Elintarvikkeiden pakkaus"],
+    tyyppi: "DEMONSTRATION" as Naytto["tyyppi"]
+  }
+]
+
+const harjoittelujaksot1 = [
+  {
+    alku: "2019-05-24",
+    loppu: "2019-07-31",
+    nimi: "Verkko-oppiminen",
+    selite: "Moodle-kurssi, viestintä",
+    tyyppi: "OTHER" as Harjoittelujakso["tyyppi"]
+  }
+]
+
+const harjoittelujaksot2 = [
+  {
+    alku: "2019-09-13",
+    loppu: "2019-09-30",
+    ohjaaja: { nimi: "John McAfee", sahkoposti: "john@mock.dev" },
+    tyotehtavat: ["Elintarvikkeiden valmistus", "Elintarvikkeiden säilytys"],
+    selite: "Palvelutalo Koivikkola",
+    tyyppi: "WORKPLACE" as Harjoittelujakso["tyyppi"]
+  },
+  {
+    alku: "2019-10-01",
+    loppu: "2019-10-15",
+    nimi: "Verkko-oppiminen",
+    selite: "Moodle-kurssi, Ikääntyminen",
+    tyyppi: "OTHER" as Harjoittelujakso["tyyppi"]
+  }
+]
+
 test("render without params", () => {
   const { getByTestId } = render(<StudyInfo />)
   expect(getByTestId("Title")).toBeEmpty()
@@ -30,7 +117,7 @@ test("render title", () => {
   expect(getByTestId("Title").textContent).toBe("Test")
 })
 
-test("render competence requirements", () => {
+test("render competence requirements", async () => {
   const { getByTestId, queryByTestId } = renderWithContext(
     <StudyInfo title="Test" competenceRequirements={osaamisvaatimukset} />
   )
@@ -38,10 +125,12 @@ test("render competence requirements", () => {
   const expandCompetences = getByTestId(
     "StudyInfo.Competences.ExpandCompetences"
   )
+
   expect(
     queryByTestId("StudyInfo.Competences.CollapseCompetences")
   ).not.toBeInTheDocument()
   expect(expandCompetences).toBeInTheDocument()
+
   fireEvent.click(expandCompetences)
 
   expect(
@@ -61,142 +150,52 @@ test("render competence requirements", () => {
   ).not.toBeInTheDocument()
 })
 
-test("render demonstrations", () => {
+test("render demonstrations", async () => {
   const {
     getByTestId,
     getAllByTestId,
     queryByTestId,
     rerender
-  } = renderWithContext(
-    <StudyInfo
-      title="Test"
-      demonstrations={[
-        {
-          alku: "2017-10-25",
-          loppu: "2017-10-26",
-          organisaatio: "Näyttöpaikka",
-          ymparisto: "Näyttöympäristö",
-          koulutuksenJarjestajaArvioijat: [
-            "Koulu Järjestäjä, koulutuksen järjestäjä"
-          ],
-          tyoelamaArvioijat: ["Työ Arvioija, työelämä"],
-          tyotehtavat: [
-            "Elintarvikkeiden valmistus",
-            "Elintarvikkeiden pakkaus"
-          ],
-          tyyppi: "DEMONSTRATION"
-        }
-      ]}
-    />
-  )
+  } = renderWithContext(<StudyInfo title="Test" demonstrations={naytot1} />)
+
   expect(getByTestId("StudyInfo.DetailsCollapsed")).toBeInTheDocument()
   expect(queryByTestId("StudyInfo.DetailsExpanded")).not.toBeInTheDocument()
   expect(getAllByTestId("StudyInfo.LearningEvent").length).toBe(1)
 
-  rerender(
-    <StudyInfo
-      title="Test"
-      demonstrations={[
-        {
-          alku: "2017-10-25",
-          loppu: "2017-10-26",
-          organisaatio: "Näyttöpaikka",
-          ymparisto: "Näyttöympäristö",
-          koulutuksenJarjestajaArvioijat: [
-            "Koulu Järjestäjä, koulutuksen järjestäjä"
-          ],
-          tyoelamaArvioijat: ["Työ Arvioija, työelämä"],
-          tyotehtavat: [
-            "Elintarvikkeiden valmistus",
-            "Elintarvikkeiden pakkaus"
-          ],
-          tyyppi: "DEMONSTRATION"
-        },
-        {
-          alku: "2017-10-25",
-          loppu: "2017-10-26",
-          organisaatio: "Näyttöpaikka",
-          ymparisto: "Näyttöympäristö",
-          koulutuksenJarjestajaArvioijat: [
-            "Koulu Järjestäjä, koulutuksen järjestäjä"
-          ],
-          tyoelamaArvioijat: ["Työ Arvioija, työelämä"],
-          tyotehtavat: [
-            "Elintarvikkeiden valmistus",
-            "Elintarvikkeiden pakkaus"
-          ],
-          tyyppi: "DEMONSTRATION"
-        }
-      ]}
-    />
-  )
+  rerender(<StudyInfo title="Test" demonstrations={naytot2} />)
 
-  expect(getAllByTestId("StudyInfo.LearningEvent").length).toBe(2)
-
-  fireEvent.click(getByTestId("StudyInfo.ExpandDetails"))
-
-  expect(queryByTestId("StudyInfo.DetailsCollapsed")).not.toBeInTheDocument()
-  expect(getByTestId("StudyInfo.DetailsExpanded")).toBeInTheDocument()
-  expect(getAllByTestId("StudyInfo.Demonstration").length).toBe(2)
+  await wait(() => {
+    expect(getAllByTestId("StudyInfo.LearningEvent").length).toBe(2)
+    fireEvent.click(getByTestId("StudyInfo.ExpandDetails"))
+    expect(queryByTestId("StudyInfo.DetailsCollapsed")).not.toBeInTheDocument()
+    expect(getByTestId("StudyInfo.DetailsExpanded")).toBeInTheDocument()
+    expect(getAllByTestId("StudyInfo.Demonstration").length).toBe(2)
+  })
 })
 
-test("render learning periods", () => {
+test("render learning periods", async () => {
   const {
     getByTestId,
     getAllByTestId,
     queryByTestId,
     rerender
   } = renderWithContext(
-    <StudyInfo
-      title="Test"
-      learningPeriods={[
-        {
-          alku: "2019-05-24",
-          loppu: "2019-07-31",
-          nimi: "Verkko-oppiminen",
-          selite: "Moodle-kurssi, viestintä",
-          tyyppi: "OTHER"
-        }
-      ]}
-    />
+    <StudyInfo title="Test" learningPeriods={harjoittelujaksot1} />
   )
+
   expect(getByTestId("StudyInfo.DetailsCollapsed")).toBeInTheDocument()
   expect(queryByTestId("StudyInfo.DetailsExpanded")).not.toBeInTheDocument()
   expect(getAllByTestId("StudyInfo.LearningEvent").length).toBe(1)
 
-  rerender(
-    <StudyInfo
-      title="Test"
-      learningPeriods={[
-        {
-          alku: "2019-09-13",
-          loppu: "2019-09-30",
-          ohjaaja: { nimi: "John McAfee", sahkoposti: "john@mock.dev" },
-          tyotehtavat: [
-            "Elintarvikkeiden valmistus",
-            "Elintarvikkeiden säilytys"
-          ],
-          selite: "Palvelutalo Koivikkola",
-          tyyppi: "WORKPLACE"
-        },
-        {
-          alku: "2019-10-01",
-          loppu: "2019-10-15",
-          nimi: "Verkko-oppiminen",
-          selite: "Moodle-kurssi, Ikääntyminen",
-          tyyppi: "OTHER"
-        }
-      ]}
-    />
-  )
+  rerender(<StudyInfo title="Test" learningPeriods={harjoittelujaksot2} />)
 
-  expect(getAllByTestId("StudyInfo.LearningEvent").length).toBe(2)
-
-  fireEvent.click(getByTestId("StudyInfo.ExpandDetails"))
-
-  expect(queryByTestId("StudyInfo.DetailsCollapsed")).not.toBeInTheDocument()
-  expect(getByTestId("StudyInfo.DetailsExpanded")).toBeInTheDocument()
-  expect(getAllByTestId("StudyInfo.LearningPeriod").length).toBe(2)
+  await wait(() => {
+    expect(getAllByTestId("StudyInfo.LearningEvent").length).toBe(2)
+    fireEvent.click(getByTestId("StudyInfo.ExpandDetails"))
+    expect(queryByTestId("StudyInfo.DetailsCollapsed")).not.toBeInTheDocument()
+    expect(getByTestId("StudyInfo.DetailsExpanded")).toBeInTheDocument()
+    expect(getAllByTestId("StudyInfo.LearningPeriod").length).toBe(2)
+  })
 })
 
 test("render verification processes", () => {
@@ -228,24 +227,7 @@ test("render verification processes", () => {
   rerender(
     <StudyInfo
       title="Title"
-      demonstrations={[
-        {
-          alku: "2019-08-01",
-          loppu: "2019-08-01",
-          organisaatio: "Näyttöpaikka",
-          ymparisto: "Näyttöympäristö",
-          koulutuksenJarjestajaArvioijat: [
-            "Olli Opettaja, koulutuksen järjestäjä",
-            "Oona Opettaja, koulutuksen järjestäjä"
-          ],
-          tyoelamaArvioijat: ["Teuvo Työpaikka, työelämä"],
-          tyotehtavat: [
-            "Elintarvikkeiden valmistus",
-            "Elintarvikkeiden pakkaus"
-          ],
-          tyyppi: "DEMONSTRATION"
-        }
-      ]}
+      demonstrations={naytot3}
       verificationProcess={{
         koodiUri: "osaamisentodentamisenprosessi_0003"
       }}
