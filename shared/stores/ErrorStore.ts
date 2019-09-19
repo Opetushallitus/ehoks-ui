3284f3b71d57b6668535fb86164dd2bbd8682edf
Tatu Niittykangas <tatu.niittykangas@gofore.com>
2019-09-19 12:35:18 +0300

    EH-549: Add clarifying comment to ErrorStore about translating errorText

--- shared/stores/ErrorStore.ts
+++ shared/stores/ErrorStore.ts
@@ -20,6 +20,8 @@ const ErrorStoreModel = {
 export const ErrorStore = types
   .model("ErrorStore", ErrorStoreModel)
   .actions(self => {
+    // logError can accept either a plain string or a translation key as the errorText parameter,
+    // see HOKSStore for an example of translation key usage.
     const logError = (id: string, errorText?: string) => {
       const error = { errorText, id, handled: false }
       self.errors.push(error as any) // https://github.com/mobxjs/mobx-state-tree/issues/501
