b47836f19971ce3610126c0bfc9e216280756085
Lari Hoppula <lari.hoppula@gofore.com>
2019-10-08 13:17:36 +0300

    EH-660: Added localisation support for saavutettavuusseloste

--- oppija/src/routes/Saavutettavuusseloste.tsx
+++ oppija/src/routes/Saavutettavuusseloste.tsx
@@ -1,5 +1,6 @@
 import { Container } from "components/Container"
 import React from "react"
+import { FormattedMessage } from "react-intl"
 import styled from "styled"
 
 const Content = styled("div")`
@@ -18,181 +19,368 @@ export function Saavutettavuusseloste(_: SaavutettavuusselosteProps) {
   return (
     <Container>
       <Content>
-        <h1>eHoks-verkkopalvelun saavutettavuusseloste</h1>
+        <h1>
+          <FormattedMessage
+            id="saavutettavuus.title"
+            defaultMessage="eHoks-verkkopalvelun saavutettavuusseloste"
+          />
+        </h1>
 
         <p>
-          Tämä saavutettavuusseloste kattaa Opintopolku.fi/eHoks-palvelun eri
+          <FormattedMessage
+            id="saavutettavuus.yleiskuvaus"
+            defaultMessage="Tämä saavutettavuusseloste kattaa Opintopolku.fi/eHoks-palvelun eri
           kieliversiot. Tämä seloste on laadittu 24.9.2019. Saavutettavuusarvio
           perustuu Opetushallituksen Oppijan palvelut -yksikön omaan
           arviointiin. Saavutettavuusarvioinnissa olemme arvioineet
-          verkkopalvelun testiversion.
+          verkkopalvelun testiversion."
+          />
         </p>
 
-        <h2>eHoks verkkopalvelu</h2>
-
-        <p>eHoks-verkkopalvelu julkaistiin 14.10.2019</p>
-
-        <h2>Saavutettavuusvaatimukset</h2>
+        <h2>
+          <FormattedMessage
+            id="saavutettavuus.ehoksVerkkopalveluTitle"
+            defaultMessage="eHoks verkkopalvelu"
+          />
+        </h2>
 
         <p>
+          <FormattedMessage
+            id="saavutettavuus.ehoksVerkkopalveluJulkaistiin"
+            defaultMessage="eHoks-verkkopalvelu julkaistiin 14.10.2019"
+          />
+        </p>
+
+        <h2>
+          <FormattedMessage
+            id="saavutettavuus.saavutettavuusvaatimuksetTitle"
+            defaultMessage="Saavutettavuusvaatimukset"
+          />
+        </h2>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.vaatimukset1"
+            defaultMessage="
           Julkishallinnon organisaatioiden verkkopalvelujen täytyy olla
-          saavutettavia.
+          saavutettavia."
+          />
         </p>
 
         <p>
-          Verkkopalvelu on saavutettava, kun käyttäjä huolimatta mahdollisista
+          <FormattedMessage
+            id="saavutettavuus.vaatimukset2"
+            defaultMessage="Verkkopalvelu on saavutettava, kun käyttäjä huolimatta mahdollisista
           toimintarajoitteista, kuten näkö- ja kuulovamma, ikä, äidinkieli tai
           kognitiiviset ja motoriset taidot, kykenee käyttämään verkkopalvelua
-          ilman ongelmia.
+          ilman ongelmia."
+          />
         </p>
 
         <p>
-          Saavutettavuudesta säädetään laissa digitaalisten palveluiden
+          <FormattedMessage
+            id="saavutettavuus.vaatimukset3"
+            defaultMessage="Saavutettavuudesta säädetään laissa digitaalisten palveluiden
           tarjoamisesta ja sitä edeltäneessä EU:n saavutettavuusdirektiivissä.
           Saavutettavuusvaatimukset perustuvat WCAG 2.1 AA -ohjeisiin. (Web
           contents accessibility guidelines, verkkopalvelujen
-          saavutettavuusohjeet).
+          saavutettavuusohjeet)."
+          />
         </p>
 
         <p>
+          <FormattedMessage
+            id="saavutettavuus.vaatimukset4"
+            defaultMessage="
           Vaatimukset koskevat eHoks-palvelussa julkaistuja sisältöjä 23.9.2019
-          alkaen.
+          alkaen."
+          />
         </p>
 
-        <h2>eHoks-verkkopalvelun saavutettavuus</h2>
+        <h2>
+          <FormattedMessage
+            id="saavutettavuus.ehoksSaavutettavuusTitle"
+            defaultMessage="eHoks-verkkopalvelun saavutettavuus"
+          />
+        </h2>
 
         <p>
+          <FormattedMessage
+            id="saavutettavus.palveluEiOleSaavutettava"
+            defaultMessage="
           Palvelu ei ole kaikille saavutettava, sillä se ei täytä lain
           edellyttämiä kriittisiä saavutettavuusvaatimuksia. Emme noudata vielä
-          lakia.
+          lakia."
+          />
         </p>
 
-        <h2>Havaitut puutteet saavutettavuudessa</h2>
+        <h2>
+          <FormattedMessage
+            id="saavutettavuus.havaitutPuutteetTitle"
+            defaultMessage="Havaitut puutteet saavutettavuudessa"
+          />
+        </h2>
 
         <ul>
           <li>
+            <FormattedMessage
+              id="saavutettavuus.puute1"
+              defaultMessage="
             Kuvien ja infograafien vaihtoehtoiset kuvaukset (alt-tekstit)
-            puuttuvat.
+            puuttuvat."
+            />
           </li>
           <li>
-            Sivujen tekstin ja taustan värikontrastit eivät ole riittäviä.
+            <FormattedMessage
+              id="saavutettavuus.puute2"
+              defaultMessage="
+            Sivujen tekstin ja taustan värikontrastit eivät ole riittäviä."
+            />
           </li>
           <li>
+            <FormattedMessage
+              id="saavutettavuus.puute3"
+              defaultMessage="
             Tekstiä esitetään kuvina ilman vaihtoehtoista kuvausta (alt-teksti),
-            esimerkiksi painikkeet ja linkit
+            esimerkiksi painikkeet ja linkit"
+            />
           </li>
           <li>
-            Käyttäjä ei voi hallinnoida palvelussa olevia aikaan sidottuja
+            <FormattedMessage
+              id="saavutettavuus.puute4"
+              defaultMessage="Käyttäjä ei voi hallinnoida palvelussa olevia aikaan sidottuja
             toimintoja. Käyttäjä ei voi kytkeä pois päältä palvelussa olevaa
-            aikarajaa, säätää sitä tai pitkittää sitä, esimerkiksi lomakepohja
+            aikarajaa, säätää sitä tai pitkittää sitä, esimerkiksi lomakepohja"
+            />
           </li>
           <li>
-            Linkkien ja lomakekenttien navigointijärjestys ei ole looginen ja
-            intuitiivinen.
+            <FormattedMessage
+              id="saavutettavuus.puute5"
+              defaultMessage="Linkkien ja lomakekenttien navigointijärjestys ei ole looginen ja
+            intuitiivinen."
+            />
           </li>
           <li>
-            Palvelu ei anna riittävästi ohjeita siitä, miten linkit toimivat.
+            <FormattedMessage
+              id="saavutettavuus.puute6"
+              defaultMessage="Palvelu ei anna riittävästi ohjeita siitä, miten linkit toimivat.
             Linkkien toimintaa ja tarkoitusta ei kerrota käyttäjälle. Käyttäjä
-            ei voi selvittää, mille sivulle ja mihin palveluun linkit aukeavat.
+            ei voi selvittää, mille sivulle ja mihin palveluun linkit aukeavat."
+            />
           </li>
           <li>
-            Näppäinkohdistus ei ole koko ajan näkyvissä eikä ole riittävän
-            näkyvä.
+            <FormattedMessage
+              id="saavutettavuus.puute7"
+              defaultMessage="Näppäinkohdistus ei ole koko ajan näkyvissä eikä ole riittävän
+            näkyvä."
+            />
           </li>
           <li>
-            Sivuston ja sivujen kielimääritykset (suomi ja ruotsi) puuttuvat.
+            <FormattedMessage
+              id="saavutettavuus.puute8"
+              defaultMessage="Sivuston ja sivujen kielimääritykset (suomi ja ruotsi) puuttuvat."
+            />
           </li>
           <li>
-            Lomakkeiden täyttämistä varten ja toiminnallisten elementtien
+            <FormattedMessage
+              id="saavutettavuus.puute9"
+              defaultMessage="Lomakkeiden täyttämistä varten ja toiminnallisten elementtien
             käyttämiseen ei ole riittävästi ohjeita. Lomakepohja on
-            puutteellinen.
+            puutteellinen."
+            />
           </li>
-          <li>Hyppää sisältöön -linkkiä ei löydy palvelun alusta.</li>
           <li>
-            Käyttäjälle ei anneta mahdollisuutta ohittaa sellaisia elementtejä,
+            <FormattedMessage
+              id="saavutettavuus.puute10"
+              defaultMessage="Hyppää sisältöön -linkkiä ei löydy palvelun alusta."
+            />
+          </li>
+          <li>
+            <FormattedMessage
+              id="saavutettavuus.puute11"
+              defaultMessage="Käyttäjälle ei anneta mahdollisuutta ohittaa sellaisia elementtejä,
             jotka eivät ole olennaisia palvelun käytölle (esimerkiksi: palvelua
-            ei ole nimetty title-tagilla).
+            ei ole nimetty title-tagilla)."
+            />
           </li>
           <li>
-            Palvelu ei ole yhteensopiva eri sovellusten kanssa eikä toimi
+            <FormattedMessage
+              id="saavutettavuus.puute12"
+              defaultMessage="Palvelu ei ole yhteensopiva eri sovellusten kanssa eikä toimi
             moitteetta avustavan teknologian avulla, esimerkiksi
-            ruudunlukuohjelmat.
+            ruudunlukuohjelmat."
+            />
           </li>
         </ul>
 
         <p>
-          eHoks-palvelun saavutettavuuteen liittyvät puutteet korjataan vuoden
+          <FormattedMessage
+            id="saavutettavuus.puutteetKorjataan"
+            defaultMessage="eHoks-palvelun saavutettavuuteen liittyvät puutteet korjataan vuoden
           2020 aikana WCAG-kriteerien mukaisesti. Tarvittaessa tarjoamme tukea
-          kaikille käyttäjille.
+          kaikille käyttäjille."
+          />
         </p>
 
-        <h2>Saavutettavuuspalaute</h2>
+        <h2>
+          <FormattedMessage
+            id="saavutettavuus.saavutettavuusPalauteTitle"
+            defaultMessage="Saavutettavuuspalaute"
+          />
+        </h2>
 
         <p>
-          Huomasitko joitakin muita saavutettavuuspuutteita eHOKS-palvelussa?
-          Kerro se meille ja teemme parhaamme puutteen korjaamiseksi.
-        </p>
-
-        <p>
-          Jos et voi käyttää jotain sisältöä, voit pyytää sen meiltä
-          saavutettavassa muodossa.
-        </p>
-
-        <p>
-          Voit antaa palautetta mahdollisista puutteista sähköpostilla
-          osoitteeseen:{" "}
-          <a href="mailto:ehoks@opintopolku.fi">ehoks@opintopolku.fi</a>
-        </p>
-
-        <p>eHoks-verkkopalvelun asiakaspalvelutiimi vastaa palautteisiin.</p>
-
-        <h2>Valvontaviranomainen</h2>
-
-        <p>
-          Jos et saa vastausta kysymyksiisi, voit tehdä puutteista valituksen
-          valvontaviranomaiselle.
+          <FormattedMessage
+            id="saavutettavuus.huomasitkoPuutteita"
+            defaultMessage="Huomasitko joitakin muita saavutettavuuspuutteita eHOKS-palvelussa?
+          Kerro se meille ja teemme parhaamme puutteen korjaamiseksi."
+          />
         </p>
 
         <p>
-          Verkkopalveluiden saavutettavuuden valvontaviranomainen on
-          Etelä-Suomen aluehallintovirasto.
+          <FormattedMessage
+            id="saavutettavuus.voitPyytaaSaavutettavassaMuodossa"
+            defaultMessage="Jos et voi käyttää jotain sisältöä, voit pyytää sen meiltä
+          saavutettavassa muodossa."
+          />
         </p>
 
         <p>
-          Jos huomaat sivustolla saavutettavuusongelmia, anna ensin palautetta
-          sivuston ylläpitäjälle. Vastaamme 14 vuorokauden sisällä.
+          <FormattedMessage
+            id="saavutettavuus.voitAntaaPalautetta"
+            defaultMessage="Voit antaa palautetta mahdollisista puutteista sähköpostilla
+          osoitteeseen:"
+          />{" "}
+          <a
+            href={`mailto:${(
+              <FormattedMessage
+                id="saavutettavuus.palauteEmail"
+                defaultMessage="ehoks@opintopolku.fi"
+              />
+            )}`}
+          >
+            <FormattedMessage
+              id="saavutettavuus.palauteEmail"
+              defaultMessage="ehoks@opintopolku.fi"
+            />
+          </a>
         </p>
 
         <p>
-          Jos et ole tyytyväinen saamaasi vastaukseen, tai et saa vastausta
+          <FormattedMessage
+            id="saavutettavuus.asiakaspalveluVastaaPalautteisiin"
+            defaultMessage="eHoks-verkkopalvelun asiakaspalvelutiimi vastaa palautteisiin."
+          />
+        </p>
+
+        <h2>
+          <FormattedMessage
+            id="saavutettavuus.valvontaviranomainenTitle"
+            defaultMessage="Valvontaviranomainen"
+          />
+        </h2>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.josEtSaaVastausta"
+            defaultMessage="Jos et saa vastausta kysymyksiisi, voit tehdä puutteista valituksen
+          valvontaviranomaiselle."
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.valvontaviranomainen"
+            defaultMessage="Verkkopalveluiden saavutettavuuden valvontaviranomainen on
+          Etelä-Suomen aluehallintovirasto."
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.josHuomaatSaavutettavuusOngelmia"
+            defaultMessage="Jos huomaat sivustolla saavutettavuusongelmia, anna ensin palautetta
+          sivuston ylläpitäjälle. Vastaamme 14 vuorokauden sisällä."
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.josEtOleTyytyvainen"
+            defaultMessage="Jos et ole tyytyväinen saamaasi vastaukseen, tai et saa vastausta
           lainkaan kahden viikon aikana, voit tehdä ilmoituksen Etelä-Suomen
-          aluehallintovirastoon.
+          aluehallintovirastoon."
+          />
         </p>
 
         <p>
-          Etelä-Suomen aluehallintoviraston sivulla kerrotaan tarkasti, miten
-          valituksen voi tehdä ja miten asia käsitellään.
+          <FormattedMessage
+            id="saavutettavuus.aluehallintovirastonSivullaKerrotaan"
+            defaultMessage="Etelä-Suomen aluehallintoviraston sivulla kerrotaan tarkasti, miten
+          valituksen voi tehdä ja miten asia käsitellään."
+          />
         </p>
 
-        <h3>Valvontaviranomaisen yhteystiedot</h3>
-
-        <p>Etelä-Suomen aluehallintovirasto</p>
-
-        <p>Saavutettavuuden valvonnan yksikkö</p>
-
-        <p>www.saavutettavuusvaatimukset.fi</p>
-
-        <p>saavutettavuus(at)avi.fi</p>
-
-        <p>puhelinnumero vaihde 0295 016 000</p>
-
-        <h3>Lue lisää</h3>
+        <h3>
+          <FormattedMessage
+            id="saavutettavuus.valvontaviranomaisenYhteystiedotTitle"
+            defaultMessage="Valvontaviranomaisen yhteystiedot"
+          />
+        </h3>
 
         <p>
-          Saavutettavuusvaatimukset.fi (Aluehallintovirasto AVI)
+          <FormattedMessage
+            id="saavutettavuus.etelaSuomenAluehallintovirasto"
+            defaultMessage="Etelä-Suomen aluehallintovirasto"
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.saavutettavuudenValvonnanYksikko"
+            defaultMessage="Saavutettavuuden valvonnan yksikkö"
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.saavutettavuusvaatimuksetPlainLink"
+            defaultMessage="www.saavutettavuusvaatimukset.fi"
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.saavutettavuusEmailPlain"
+            defaultMessage="saavutettavuus(at)avi.fi"
+          />
+        </p>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.puhelinnumero"
+            defaultMessage="puhelinnumero vaihde 0295 016 000"
+          />
+        </p>
+
+        <h3>
+          <FormattedMessage
+            id="saavutettavuus.lueLisaaTitle"
+            defaultMessage="Lue lisää"
+          />
+        </h3>
+
+        <p>
+          <FormattedMessage
+            id="saavutettavuus.saavutettavuusvaatimuksetDescription"
+            defaultMessage="Saavutettavuusvaatimukset.fi (Aluehallintovirasto AVI)"
+          />
           <br />
           <a href="https://www.saavutettavuusvaatimukset.fi/">
-            https://www.saavutettavuusvaatimukset.fi/
+            <FormattedMessage
+              id="saavutettavuus.saavutettavuusvaatimuksetLink"
+              defaultMessage="https://www.saavutettavuusvaatimukset.fi/"
+            />
           </a>
         </p>
       </Content>
