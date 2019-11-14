6705d6ac41a4676b4fc3719c824b950eb7228e34
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-11-14 09:10:41 +0200

    Removed unused property from oto

--- shared/models/OpiskeluvalmiuksiaTukevatOpinnot.ts
+++ shared/models/OpiskeluvalmiuksiaTukevatOpinnot.ts
@@ -1,7 +1,6 @@
 import { types } from "mobx-state-tree"
 
 export const OpiskeluvalmiuksiaTukevatOpinnot = types.model("OpiskeluvalmiuksiaTukevatOpinnotModel", {
-  id: types.optional(types.number, 0),
   nimi: types.optional(types.string, ""),
   kuvaus: types.optional(types.string, ""),
   alku: types.optional(types.string, ""),
