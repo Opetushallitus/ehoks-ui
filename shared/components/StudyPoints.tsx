101e755889b78f786683092c8c4fd4380025e0a7
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-11-22 10:06:50 +0200

    StudyPoints component

---
+++ shared/components/StudyPoints.tsx
@@ -0,0 +1,44 @@
+import { FormattedMessage } from "react-intl"
+import { LabeledColumn } from "./LabeledColumn"
+import React from "react"
+
+const Points = (props: {
+  osaamispisteet?: number
+  pointsTranslationId: string
+}) => {
+  if (props.osaamispisteet) {
+    return (
+      <>
+        {props.osaamispisteet}{" "}
+        <FormattedMessage
+          id={props.pointsTranslationId}
+          defaultMessage="osaamispistettä"
+        />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <FormattedMessage
+        id="osaamispisteet.laajuusNull"
+        defaultMessage="Tutkinnon laajuus ei tiedossa"
+      />
+    </>
+  )
+}
+
+export const StudyPoints = (props: {
+  osaamispisteet?: number
+  titleTranslationId: string
+  pointsTranslationId: string
+}) => {
+  return (
+    <LabeledColumn id={props.titleTranslationId}>
+      <Points
+        osaamispisteet={props.osaamispisteet}
+        pointsTranslationId={props.pointsTranslationId}
+      />
+    </LabeledColumn>
+  )
+}
