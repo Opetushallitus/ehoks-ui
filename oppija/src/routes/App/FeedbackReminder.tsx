91004918af27faddab44f40aaec4bcba82c0ba85
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2019-12-05 12:47:13 +0200

    Added margin for feedback alarm icon

--- oppija/src/routes/App/FeedbackReminder.tsx
+++ oppija/src/routes/App/FeedbackReminder.tsx
@@ -4,8 +4,13 @@ import styled from "styled"
 
 const StyledAlarm = styled(MdAlarm)`
   cursor: pointer;
-  width: 70px;
-  height: 70px;
+  width: 64px;
+  height: 64px;
+  margin: 4px 0 4px 30px;
+
+  @media screen and (max-width: ${props => props.theme.breakpoints.Desktop}px) {
+    margin: 4px 0 4px 20px;
+  }
 `
 
 interface FeedbackReminderProps {
