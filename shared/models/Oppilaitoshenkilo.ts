30808929517b81f487eaecb35454d95c266eb5eb
Tatu Niittykangas <tatu.niittykangas@gofore.com>
2020-01-08 14:17:44 +0200

    EH-757: Refactor OppilaitosHenkilo to use new organisaatio oid helper

--- shared/models/Oppilaitoshenkilo.ts
+++ shared/models/Oppilaitoshenkilo.ts
@@ -1,8 +1,39 @@
-import { types } from "mobx-state-tree"
+import { getRoot, types } from "mobx-state-tree"
+import { EnrichOrganisaatioOid } from "./EnrichOrganisaatioOid";
+import { LocaleRoot } from "./helpers/LocaleRoot";
 
-export const Oppilaitoshenkilo = types.model("Oppilaitoshenkilo", {
+const Organisaatio = types.model("Organisaatio", {
+  oid: types.optional(types.string, ""),
+  nimi: types.optional(
+    types.model({
+       fi: types.optional(types.string, ""),
+       sv: types.optional(types.string, "")
+     }),
+    {}
+  )
+})
+
+const Model = types.model("OppilaitoshenkiloModel", {
   id: types.optional(types.number, 0),
   nimi: types.optional(types.string, ""),
   rooli: types.optional(types.string, ""),
-  oppilaitosOid: types.optional(types.string, "")
+  oppilaitosOid: types.optional(types.string, ""),
+  oppilaitos: types.optional(Organisaatio, {})
 })
+
+export const Oppilaitoshenkilo = types
+    .compose(
+      "Oppilaitoshenkilo",
+      EnrichOrganisaatioOid,
+      Model
+    )
+    .views(self => {
+        const root: LocaleRoot = getRoot(self)
+        return {
+            get oppilaitosNimi() {
+              return self.oppilaitos.nimi && self.oppilaitos.nimi[root.translations.activeLocale]
+                ? self.oppilaitos.nimi[root.translations.activeLocale]
+                : ""
+            }
+        }
+    })
