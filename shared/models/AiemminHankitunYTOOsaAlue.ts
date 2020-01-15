d7013307718bae8c02b2431f16a63c29a269bd66
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-01-15 14:18:51 +0200

    Add enriched osaAlue and valittuTodentamisenProsessi for ahytoOsaAlue

--- shared/models/AiemminHankitunYTOOsaAlue.ts
+++ shared/models/AiemminHankitunYTOOsaAlue.ts
@@ -1,17 +1,46 @@
 import { types } from "mobx-state-tree"
 import { OsaamisenOsoittaminen } from "./OsaamisenOsoittaminen"
 import { TodennettuArviointiLisatiedot } from "./TodennettuArviointiLisatiedot"
+import { TutkinnonOsaViews } from "./helpers/TutkinnonOsaViews"
+import { EnrichKoodiUri } from "./EnrichKoodiUri"
+import { KoodistoVastaus } from "./KoodistoVastaus"
+import { getOtsikko } from "./helpers/getOtsikko"
 
-export const AiemminHankitunYTOOsaAlue = types.model(
-  "AiemminHankitunYTOOsaAlue",
-  {
+export const Model = types.model(
+  "AiemminHankitunYTOOsaAlue", {
     id: types.optional(types.number, 0),
     olennainenSeikka: types.optional(types.boolean, false),
     osaAlueKoodiUri: types.optional(types.string, ""),
+    osaAlue: types.optional(KoodistoVastaus, {}),
     koulutuksenJarjestajaOid: types.optional(types.string, ""),
     vaatimuksistaTaiTavoitteistaPoikkeaminen: types.optional(types.string, ""),
     valittuTodentamisenProsessiKoodiUri: types.optional(types.string, ""),
+    valittuTodentamisenProsessi: types.optional(KoodistoVastaus, {}),
     tarkentavatTiedotNaytto: types.array(OsaamisenOsoittaminen),
     tarkentavatTiedotArvioija: types.optional(TodennettuArviointiLisatiedot, {})
   }
 )
+
+export const AiemminHankitunYTOOsaAlue = types
+  .compose(
+    EnrichKoodiUri,
+    Model,
+    TutkinnonOsaViews
+  )
+  .views(self => {
+    return {
+      get otsikko() {
+        return self.osaAlue ? self.osaAlue.nimi : ""
+      },
+      get osaamispisteet() {
+        // TODO: where do we get this?
+        return 0
+      },
+      opintoOtsikko(ospLyhenne: string): string {
+        return getOtsikko(this, ospLyhenne)
+      }
+    }
+  })
+
+
+
