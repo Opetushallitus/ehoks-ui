86bf86e5c676af47045daf393b1ab0aeaab6ec19
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-02-18 13:48:57 +0200

    Removed final unused view from oppija

--- oppija/src/models/Oppija.ts
+++ oppija/src/models/Oppija.ts
@@ -1,21 +1,7 @@
 import { types } from "mobx-state-tree"
-import { Harjoittelujakso } from "models/helpers/TutkinnonOsa"
 import { HOKS } from "models/HOKS"
 
-export const Oppija = types
-  .model("Oppija", {
-    nimi: types.optional(types.string, ""),
-    hoks: types.optional(HOKS, {})
-  })
-  .views(self => {
-    return {
-      get harjoittelujakso(): Harjoittelujakso {
-        const { hankittavatTutkinnonOsat } = self.hoks
-        return hankittavatTutkinnonOsat.length &&
-          hankittavatTutkinnonOsat[0].harjoittelujaksot &&
-          hankittavatTutkinnonOsat[0].harjoittelujaksot.length
-          ? hankittavatTutkinnonOsat[0].harjoittelujaksot[0]
-          : {}
-      }
-    }
-  })
+export const Oppija = types.model("Oppija", {
+  nimi: types.optional(types.string, ""),
+  hoks: types.optional(HOKS, {})
+})
