2805957d834eb448e834d59ba478da92e35390d5
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-02-18 13:45:14 +0200

    Removed unused view from oppija

--- oppija/src/models/Oppija.ts
+++ oppija/src/models/Oppija.ts
@@ -1,5 +1,3 @@
-import format from "date-fns/format"
-import parseISO from "date-fns/parseISO"
 import { types } from "mobx-state-tree"
 import { Harjoittelujakso } from "models/helpers/TutkinnonOsa"
 import { HOKS } from "models/HOKS"
@@ -18,15 +16,6 @@ export const Oppija = types
           hankittavatTutkinnonOsat[0].harjoittelujaksot.length
           ? hankittavatTutkinnonOsat[0].harjoittelujaksot[0]
           : {}
-      },
-      get ajankohta() {
-        if (!this.harjoittelujakso.alku || !this.harjoittelujakso.loppu) {
-          return ""
-        }
-        return [
-          format(parseISO(this.harjoittelujakso.alku || ""), "d.M."),
-          format(parseISO(this.harjoittelujakso.loppu || ""), "d.M.yyyy")
-        ].join("-")
       }
     }
   })
