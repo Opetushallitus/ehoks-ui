45cf707f05f3062fcf4001d600a2d82d1a1b79e7
Tatu Niittykangas <tatu.niittykangas@gofore.com>
2020-03-05 15:18:00 +0200

    EH-793 Update docker scripts to create log folder and direct access and error logs there.

--- oppija/scripts/ci/run.sh
+++ oppija/scripts/ci/run.sh
@@ -29,6 +29,9 @@ unset config_json
 echo "Starting Prometheus node_exporter…"
 nohup /usr/local/bin/node_exporter > /root/node_exporter.log 2>&1 &
 
+echo "Create log directory…"
+mkdir -p /home/oph/logs
+
 echo "Starting nginx…"
 mkdir -p /tmp/var/nginx
 mkdir -p /tmp/run/nginx
