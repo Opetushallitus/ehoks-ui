6f3d149100ebb9ebaf616408e19be0cf2619a5a3
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-03-10 10:52:52 +0200

    Temporary structure to replace naytto with osaamisenOsoittaminen

--- shared/components/StudyInfo/Demonstration.tsx
+++ shared/components/StudyInfo/Demonstration.tsx
@@ -4,7 +4,11 @@ import { FormattedMessage } from "react-intl"
 import { HMediaQuery } from "responsive"
 import styled from "styled"
 import { Container, InfoContainer, Table, TBody, TD, TH, Title } from "./Shared"
-import { Naytto, TodentamisenProsessi } from "models/helpers/TutkinnonOsa"
+import {
+  Naytto,
+  OsaamisenOsoittaminen,
+  TodentamisenProsessi
+} from "models/helpers/TutkinnonOsa"
 import { LearningEvent } from "components/StudyInfo/LearningEvent"
 import { VerificationProcess } from "types/VerificationProcess"
 import { HeroButton } from "components/Button"
@@ -201,6 +205,165 @@ export class Demonstration extends React.Component<
           requirements={demonstration.yksilollisetKriteerit}
           deviations={demonstration.vaatimuksistaTaiTavoitteistaPoikkeaminen}
         />
+      </Container>
+    )
+  }
+}
+
+interface DemonstrationPropsTEMP {
+  demonstration?: Naytto
+  demonstrationTEMP: OsaamisenOsoittaminen
+  verificationProcess?: TodentamisenProsessi
+  koodiUri?: string
+  hasActiveShare?: boolean
+}
+
+export class DemonstrationTEMP extends React.Component<
+  DemonstrationPropsTEMP,
+  DemonstrationState
+> {
+  static contextType = AppContext
+  declare context: React.ContextType<typeof AppContext>
+
+  state: DemonstrationState = {
+    requirementsAndDeviationsExpanded: false
+  }
+
+  toggleRequirementsAndDeviations = () => {
+    this.setState(state => ({
+      requirementsAndDeviationsExpanded: !state.requirementsAndDeviationsExpanded
+    }))
+  }
+
+  share = () => {
+    const { koodiUri } = this.props
+    if (koodiUri) {
+      navigate(
+        `${window.location.pathname}?${stringifyShareParams({
+          share: koodiUri,
+          type: "naytto"
+        })}`
+      )
+    }
+  }
+
+  render() {
+    const {
+      // demonstration,
+      demonstrationTEMP,
+      hasActiveShare = false,
+      verificationProcess
+    } = this.props
+    const { featureFlags } = this.context
+    // const { requirementsAndDeviationsExpanded } = this.state
+
+    const title =
+      verificationProcess &&
+      verificationProcess.koodiUri === VerificationProcess.OHJAUS_NAYTTOON ? (
+        <FormattedMessage
+          id="opiskelusuunnitelma.osaaminenOsoitetaanNaytossaTitle"
+          defaultMessage="Osaaminen osoitetaan näytössä"
+        />
+      ) : (
+        <FormattedMessage
+          id="opiskelusuunnitelma.nayttoTitle"
+          defaultMessage="Näyttö"
+        />
+      )
+
+    const showShareButton = !hasActiveShare && featureFlags.shareDialog
+
+    const organizationName = demonstrationTEMP.nayttoymparisto?.nimi
+    // TODO siirra modeliin
+    const learningEventType = "DEMONSTRATION"
+
+    return (
+      <Container data-testid="StudyInfo.Demonstration">
+        <DemonstrationTitle>
+          <FlexLearningEvent
+            title={title}
+            type={learningEventType}
+            description={organizationName}
+            startDate={demonstrationTEMP.alku}
+            endDate={demonstrationTEMP.loppu}
+            size="large"
+          />
+          {showShareButton && (
+            <ButtonContainer>
+              <Button onClick={this.share}>
+                <FormattedMessage
+                  id="jakaminen.jaaNaytonTiedotButtonTitle"
+                  defaultMessage="Näytön tietojen jakaminen"
+                />
+                <ShareIcon size={24} />
+              </Button>
+            </ButtonContainer>
+          )}
+        </DemonstrationTitle>
+        {/*<DemonstrationTable>*/}
+        {/*  <TBody>*/}
+        {/*    <tr>*/}
+        {/*      <TH>*/}
+        {/*        <FormattedMessage*/}
+        {/*          id="opiskelusuunnitelma.nayttoymparistoTitle"*/}
+        {/*          defaultMessage="Näyttöympäristö"*/}
+        {/*        />*/}
+        {/*      </TH>*/}
+        {/*      <TD>{demonstration.ymparisto}</TD>*/}
+        {/*    </tr>*/}
+        {/*    <tr>*/}
+        {/*      <TH>*/}
+        {/*        <FormattedMessage*/}
+        {/*          id="opiskelusuunnitelma.arvioijatTitle"*/}
+        {/*          defaultMessage="Näytön arvioijat"*/}
+        {/*        />*/}
+        {/*      </TH>*/}
+        {/*      <TD>*/}
+        {/*        {demonstration &&*/}
+        {/*          demonstration.koulutuksenJarjestajaArvioijat &&*/}
+        {/*          demonstration.koulutuksenJarjestajaArvioijat.map(*/}
+        {/*            (arvioija, i) => (*/}
+        {/*              <span key={i}>*/}
+        {/*                {arvioija} <br />*/}
+        {/*              </span>*/}
+        {/*            )*/}
+        {/*          )}*/}
+        {/*        {demonstration &&*/}
+        {/*          demonstration.tyoelamaArvioijat &&*/}
+        {/*          demonstration.tyoelamaArvioijat.map((arvioija, i) => (*/}
+        {/*            <span key={i}>*/}
+        {/*              {arvioija} <br />*/}
+        {/*            </span>*/}
+        {/*          ))}*/}
+        {/*      </TD>*/}
+        {/*    </tr>*/}
+        {/*  </TBody>*/}
+        {/*</DemonstrationTable>*/}
+        {/*<HMediaQuery.MaxWidth breakpoint="Tablet">*/}
+        {/*  <CustomSlider>*/}
+        {/*    {demonstration &&*/}
+        {/*      demonstration.tyotehtavat &&*/}
+        {/*      demonstration.tyotehtavat.map((tyotehtava, i) => {*/}
+        {/*        return <Slide key={i}>{tyotehtava}</Slide>*/}
+        {/*      })}*/}
+        {/*  </CustomSlider>*/}
+        {/*</HMediaQuery.MaxWidth>*/}
+        {/*<HMediaQuery.MaxWidth breakpoint="Tablet" notMatch>*/}
+        {/*  <DemonstrationTasks>*/}
+        {/*    {demonstration &&*/}
+        {/*      demonstration.tyotehtavat &&*/}
+        {/*      demonstration.tyotehtavat.map((tyotehtava, i) => {*/}
+        {/*        return <li key={i}>{tyotehtava}</li>*/}
+        {/*      })}*/}
+        {/*  </DemonstrationTasks>*/}
+        {/*</HMediaQuery.MaxWidth>*/}
+
+        {/*<RequirementsAndDeviations*/}
+        {/*  toggle={this.toggleRequirementsAndDeviations}*/}
+        {/*  expanded={requirementsAndDeviationsExpanded}*/}
+        {/*  requirements={demonstration.yksilollisetKriteerit}*/}
+        {/*  deviations={demonstration.vaatimuksistaTaiTavoitteistaPoikkeaminen}*/}
+        {/*/>*/}
       </Container>
     )
   }
