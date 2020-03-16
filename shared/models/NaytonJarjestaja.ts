4b1abc8bded53a2aa375d2c4776957a8ef6ced1c
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-03-16 13:28:57 +0200

    Added view to NaytonJarjestaja to support different locales

--- shared/models/NaytonJarjestaja.ts
+++ shared/models/NaytonJarjestaja.ts
@@ -1,6 +1,7 @@
-import { types } from "mobx-state-tree"
+import { getRoot, types } from "mobx-state-tree"
 import { Organisaatio } from "./Organisaatio"
 import { EnrichOrganisaatioOid } from "./EnrichOrganisaatioOid"
+import { LocaleRoot } from "./helpers/LocaleRoot"
 
 const Model = types.model("NaytonJarjestajaModel", {
   id: types.optional(types.number, 0),
@@ -8,8 +9,16 @@ const Model = types.model("NaytonJarjestajaModel", {
   oppilaitos: types.optional(Organisaatio, {})
 })
 
-export const NaytonJarjestaja = types.compose(
-  "NaytonJarjestaja",
-  EnrichOrganisaatioOid,
-  Model
-)
+export const NaytonJarjestaja = types
+  .compose("NaytonJarjestaja", EnrichOrganisaatioOid, Model)
+  .views(self => {
+    const root: LocaleRoot = getRoot(self)
+    return {
+      get oppilaitosNimi() {
+        return self.oppilaitos.nimi &&
+          self.oppilaitos.nimi[root.translations.activeLocale]
+          ? self.oppilaitos.nimi[root.translations.activeLocale]
+          : ""
+      }
+    }
+  })
