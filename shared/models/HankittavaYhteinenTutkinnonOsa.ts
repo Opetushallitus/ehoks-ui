16a398c2b8b89b37f3bbaa6a7329d0d9d25fc37a
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-03-25 13:43:58 +0200

    Removed koulutuksenJarjestaja from hankittava yhteinen tutkinnon osa as this model isn't what's shown at the ui (osaAlueet is shown instead) and thus we would make unnecessary organisaatio-palvelu request.

--- shared/models/HankittavaYhteinenTutkinnonOsa.ts
+++ shared/models/HankittavaYhteinenTutkinnonOsa.ts
@@ -2,20 +2,16 @@ import { types } from "mobx-state-tree"
 import { YhteisenTutkinnonOsanOsaAlue } from "./YhteisenTutkinnonOsanOsaAlue"
 import { EnrichKoodiUri } from "models/EnrichKoodiUri"
 import { EPerusteetVastaus } from "models/EPerusteetVastaus"
-import { EnrichOrganisaatioOid } from "./EnrichOrganisaatioOid"
-import { Organisaatio } from "./Organisaatio"
 
 export const Model = types.model({
   tutkinnonOsaKoodiUri: types.optional(types.string, ""),
   tutkinnonOsa: types.optional(EPerusteetVastaus, {}),
   koulutuksenJarjestajaOid: types.optional(types.string, ""),
-  koulutuksenJarjestaja: types.maybe(Organisaatio),
   osaAlueet: types.array(YhteisenTutkinnonOsanOsaAlue)
 })
 
 export const HankittavaYhteinenTutkinnonOsa = types.compose(
   "HankittavaYhteinenTutkinnonOsa",
   EnrichKoodiUri,
-  EnrichOrganisaatioOid("koulutuksenJarjestajaOid"),
   Model
 )
