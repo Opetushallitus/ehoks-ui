1be474bd036507cc0138d324f3e65a6f204b06a7
Joonas Kallunki <jkallunki@gmail.com>
2020-03-27 12:14:21 +0200

    Update tests for organisaatioNimi

--- shared/components/__tests__/StudyInfo.test.tsx
+++ shared/components/__tests__/StudyInfo.test.tsx
@@ -255,12 +255,20 @@ describe("StudyInfo", () => {
           koodiUri: "osaamisentodentamisenprosessi_0002",
           lahetettyArvioitavaksi: "2019-04-15"
         }}
+        educationOrganizer={{
+          oid: "1.2.246.562.10.5921222",
+          nimi: { fi: "Koulutuksen järjestäjä-organisaatio", sv: "" },
+          organisaatioNimi: "Koulutuksen järjestäjä-organisaatio"
+        }}
       />
     )
 
     expect(queryAllByTestId("StudyInfo.LearningEvent").length).toBe(0)
     expect(
       queryByTestId("StudyInfo.AssessmentVerification")
+    ).toBeInTheDocument()
+    expect(
+      queryByTestId("StudyInfo.AssessmentVerificationOrganisation")
     ).toBeInTheDocument()
 
     rerender(
