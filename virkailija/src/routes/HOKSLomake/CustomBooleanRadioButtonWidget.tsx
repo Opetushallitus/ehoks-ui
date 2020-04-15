dde8936954b071de139d5f3b676a8df4a6ad6053
Joonas Kallunki <jkallunki@gmail.com>
2020-04-15 10:15:17 +0300

    React-jsonschema-form custom components use autofocus

--- virkailija/src/routes/HOKSLomake/CustomBooleanRadioButtonWidget.tsx
+++ virkailija/src/routes/HOKSLomake/CustomBooleanRadioButtonWidget.tsx
@@ -22,6 +22,8 @@ export function CustomBooleanRadioButtonWidget(props: WidgetProps) {
               name={name}
               required={required}
               value={option.value}
+              // The autoFocus prop should not be used, as it can reduce usability and accessibility for users
+              // eslint-disable-next-line jsx-a11y/no-autofocus
               autoFocus={autofocus && i === 0}
               onChange={() => onChange(option.value)}
             />
