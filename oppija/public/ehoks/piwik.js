f57edec7d9688a95358041dcbc4efb156f4185b2
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-04-15 15:46:56 +0300

    eh-817 add piwik if to script

--- oppija/public/ehoks/piwik.js
+++ oppija/public/ehoks/piwik.js
@@ -1,25 +1,27 @@
+function piwik(siteId) {
+  var _paq = window._paq || []
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["trackPageView"])
+  _paq.push(["enableLinkTracking"])
+  ;(function() {
+    var u = "//analytiikka.opintopolku.fi/matomo/"
+    _paq.push(["setTrackerUrl", u + "matomo.php"])
+    _paq.push(["setSiteId", siteId])
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0]
+    g.type = "text/javascript"
+    g.async = true
+    g.defer = true
+    g.src = u + "matomo.js"
+    s.parentNode.insertBefore(g, s)
+  })()
+}
+
 if (document.URL.indexOf("testiopintopolku.fi") != -1) {
-  var siteId = "23"
+  piwik("23")
 } else if (document.URL.indexOf("opintopolku.fi") != -1) {
-  var siteId = "22"
+  piwik("22")
 } else {
   console.log("No siteId for Matomo, hence no data collection")
 }
-
-var _paq = window._paq || []
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["trackPageView"])
-_paq.push(["enableLinkTracking"])
-;(function() {
-  var u = "//analytiikka.opintopolku.fi/matomo/"
-  _paq.push(["setTrackerUrl", u + "matomo.php"])
-  _paq.push(["setSiteId", siteId])
-  var d = document,
-    g = d.createElement("script"),
-    s = d.getElementsByTagName("script")[0]
-  g.type = "text/javascript"
-  g.async = true
-  g.defer = true
-  g.src = u + "matomo.js"
-  s.parentNode.insertBefore(g, s)
-})()
