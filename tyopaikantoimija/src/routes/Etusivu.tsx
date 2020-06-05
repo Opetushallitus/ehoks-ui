2ad5bc978408e03b2353fdecf605629c46b3bb6c
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-06-05 10:25:57 +0300

    cosmetic change to test new build

--- tyopaikantoimija/src/routes/Etusivu.tsx
+++ tyopaikantoimija/src/routes/Etusivu.tsx
@@ -28,7 +28,7 @@ export class Etusivu extends React.Component<EtusivuProps> {
         <Header>
           <FormattedMessage
             id="etusivu.tyopaikantoimijantitle"
-            defaultMessage="Työpaikantoimijan etusivu"
+            defaultMessage="Työpaikantoimijan Etusivu"
           />
         </Header>
 
