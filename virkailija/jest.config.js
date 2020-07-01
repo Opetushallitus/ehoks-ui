29cd4454b47b69260b6c9e3af242547537bb5a5f
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-07-01 10:24:33 +0300

    Made Jest search tests only from typescript files. Default for testMatch is [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ] meaning js files would be searched also but we don't have js tests.

--- virkailija/jest.config.js
+++ virkailija/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/../shared/setupTests.ts"],
   modulePaths: ["<rootDir>/src", "<rootDir>/../shared"],
   roots: ["<rootDir>", "<rootDir>/../shared"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
   moduleNameMapper: {
     "\\.(css|jpg|png|svg)$": "<rootDir>/empty-module.js"
   }
