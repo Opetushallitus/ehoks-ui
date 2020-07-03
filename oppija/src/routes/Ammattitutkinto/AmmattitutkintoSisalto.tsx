b01b3776425814099c74184001c7682878a6b20b
Joonas Kallunki <jkallunki@gmail.com>
2020-07-03 09:31:02 +0300

    Component without props should be function component

--- oppija/src/routes/Ammattitutkinto/AmmattitutkintoSisalto.tsx
+++ oppija/src/routes/Ammattitutkinto/AmmattitutkintoSisalto.tsx
@@ -9,39 +9,37 @@ const OpintopolkuLink = styled("a")`
   color: ${props => props.theme.colors.waterBlue};
 `
 
-export class AmmattitutkintoSisalto extends React.Component<any> {
-  render = () => (
-    <Section>
-      <SectionTitle>
-        <FormattedMessage
-          id="ammattitutkinto.sisaltoTitle"
-          defaultMessage="Mitä ammatilliset tutkinnot sisältävät?"
-        />
-      </SectionTitle>
-      <p>
-        <FormattedMessage
-          id="ammattitutkinto.ensimmainenKappale"
-          defaultMessage="Ammatillisessa koulutuksessa hankit osaamista työelämää varten. Ammatillisia tutkintoja ovat ammatilliset perustutkinnot, ammattitutkinnot ja erikoisammattitutkinnot. Ammatillisessa perustutkinnossa osoitat laaja-alaiset ammatilliset perusvalmiudet alan eri tehtäviin. Lisäksi voit saada valmiuksia yrittäjyyteen sekä jatko-opintojen kannalta tarpeellisia tietoja ja taitoja. Ammatillinen perustutkinto 180 osaamispisteen laajuinen tutkinto."
-        />
-      </p>
-      <p>
-        <FormattedMessage
-          id="ammattitutkinto.toinenKappale"
-          defaultMessage="Ammattitutkinnossa osoitat työelämän tarpeiden mukaisesti kohdennettua ammattitaitoa, joka on perustutkintoa syvällisempää tai kohdistuu rajatumpiin työtehtäviin. Ammattitutkinto on 150 tai 120 osaamispisteen laajuinen tutkinto. Erikoisammattitutkinnossa osoitat ammattitutkintoa syvällisempää ammatin hallintaa tai monialaista osaamista. Erikoisammattitutkinto on 180 osaamispisteen laajuinen tutkinto."
-        />
-      </p>
-      <p>
-        <FormattedMessage
-          id="ammattitutkinto.kolmasKappale"
-          defaultMessage="Lisää tietoa ammatillisesta koulutuksesta"
-        />
-        <OpintopolkuLink
-          href="https://opintopolku.fi/wp/ammatillinen-koulutus/"
-          target="_blank"
-        >
-          https://opintopolku.fi/wp/ammatillinen-koulutus/
-        </OpintopolkuLink>
-      </p>
-    </Section>
-  )
-}
+export const AmmattitutkintoSisalto = () => (
+  <Section>
+    <SectionTitle>
+      <FormattedMessage
+        id="ammattitutkinto.sisaltoTitle"
+        defaultMessage="Mitä ammatilliset tutkinnot sisältävät?"
+      />
+    </SectionTitle>
+    <p>
+      <FormattedMessage
+        id="ammattitutkinto.ensimmainenKappale"
+        defaultMessage="Ammatillisessa koulutuksessa hankit osaamista työelämää varten. Ammatillisia tutkintoja ovat ammatilliset perustutkinnot, ammattitutkinnot ja erikoisammattitutkinnot. Ammatillisessa perustutkinnossa osoitat laaja-alaiset ammatilliset perusvalmiudet alan eri tehtäviin. Lisäksi voit saada valmiuksia yrittäjyyteen sekä jatko-opintojen kannalta tarpeellisia tietoja ja taitoja. Ammatillinen perustutkinto 180 osaamispisteen laajuinen tutkinto."
+      />
+    </p>
+    <p>
+      <FormattedMessage
+        id="ammattitutkinto.toinenKappale"
+        defaultMessage="Ammattitutkinnossa osoitat työelämän tarpeiden mukaisesti kohdennettua ammattitaitoa, joka on perustutkintoa syvällisempää tai kohdistuu rajatumpiin työtehtäviin. Ammattitutkinto on 150 tai 120 osaamispisteen laajuinen tutkinto. Erikoisammattitutkinnossa osoitat ammattitutkintoa syvällisempää ammatin hallintaa tai monialaista osaamista. Erikoisammattitutkinto on 180 osaamispisteen laajuinen tutkinto."
+      />
+    </p>
+    <p>
+      <FormattedMessage
+        id="ammattitutkinto.kolmasKappale"
+        defaultMessage="Lisää tietoa ammatillisesta koulutuksesta"
+      />
+      <OpintopolkuLink
+        href="https://opintopolku.fi/wp/ammatillinen-koulutus/"
+        target="_blank"
+      >
+        https://opintopolku.fi/wp/ammatillinen-koulutus/
+      </OpintopolkuLink>
+    </p>
+  </Section>
+)
