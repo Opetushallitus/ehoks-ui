57aa3816049c1fdfbc69dbaee4349d56a96f3836
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-08-13 14:16:56 +0300

    EH-747: Aiemmin hankitut yhteiset to use osa-alue enrichment

--- shared/models/YhteinenTutkinnonOsa/AiemminHankittuYhteinenTutkinnonOsa.ts
+++ shared/models/YhteinenTutkinnonOsa/AiemminHankittuYhteinenTutkinnonOsa.ts
@@ -5,23 +5,30 @@ import { AiemminHankitunYTOOsaAlue } from "./AiemminHankitunYTOOsaAlue"
 import { EPerusteetVastaus } from "models/EPerusteetVastaus"
 import { TutkinnonOsaViite } from "models/TutkinnonOsaViite"
 import { TutkinnonOsaType } from "../helpers/ShareTypes"
+import { EnrichTutkinnonOsaAndOsaAlue } from "../Enrichment/EnrichTutkinnonOsaAndOsaAlue"
+
+const Model = types.model({
+  id: types.optional(types.number, 0),
+  moduleId: types.maybe(types.string),
+  tutkinnonOsaKoodiUri: types.optional(types.string, ""),
+  tutkinnonOsa: types.optional(EPerusteetVastaus, {}),
+  tutkinnonOsaViitteet: types.array(TutkinnonOsaViite),
+  koulutuksenJarjestajaOid: types.optional(types.string, ""),
+  osaAlueet: types.array(AiemminHankitunYTOOsaAlue),
+  valittuTodentamisenProsessiKoodiUri: types.optional(types.string, ""),
+  tarkentavatTiedotNaytto: types.array(OsaamisenOsoittaminen),
+  tarkentavatTiedotOsaamisenArvioija: types.optional(
+    TodennettuArviointiLisatiedot,
+    {}
+  )
+})
 
 export const AiemminHankittuYhteinenTutkinnonOsa = types
-  .model("AiemminHankittuYhteinenTutkinnonOsa", {
-    id: types.optional(types.number, 0),
-    moduleId: types.maybe(types.string),
-    tutkinnonOsaKoodiUri: types.optional(types.string, ""),
-    tutkinnonOsa: types.optional(EPerusteetVastaus, {}),
-    tutkinnonOsaViitteet: types.array(TutkinnonOsaViite),
-    koulutuksenJarjestajaOid: types.optional(types.string, ""),
-    osaAlueet: types.array(AiemminHankitunYTOOsaAlue),
-    valittuTodentamisenProsessiKoodiUri: types.optional(types.string, ""),
-    tarkentavatTiedotNaytto: types.array(OsaamisenOsoittaminen),
-    tarkentavatTiedotOsaamisenArvioija: types.optional(
-      TodennettuArviointiLisatiedot,
-      {}
-    )
-  })
+  .compose(
+    "AiemminHankittuYhteinenTutkinnonOsa",
+    EnrichTutkinnonOsaAndOsaAlue,
+    Model
+  )
   .views(self => ({
     get tutkinnonOsaTyyppi(): TutkinnonOsaType {
       return TutkinnonOsaType.AiemminHankittuYhteinen
