5cb89ddcd5d1e61e0796fedf6a6e54162436ad16
samje <sami.honkanen@cybercom.com>
2021-04-07 15:25:46 +0300

    EH-1133: Add oppisopimuksen perusta to create/edit hoks

--- virkailija/src/routes/MuokkaaHOKS/uiSchema.ts
+++ virkailija/src/routes/MuokkaaHOKS/uiSchema.ts
@@ -563,6 +563,8 @@ const fullUiSchema = (options: UiSchemaOptions): { [key: string]: any } => ({
           "ui:order": [
             "osaamisen-hankkimistapa-koodi-uri",
             "osaamisen-hankkimistapa-koodi-versio",
+            "oppisopimuksen-perusta-uri",
+            "oppisopimuksen-perusta-versio",
             "alku",
             "loppu",
             "ajanjakson-tarkenne",
@@ -583,6 +585,13 @@ const fullUiSchema = (options: UiSchemaOptions): { [key: string]: any } => ({
             typeahead: typeaheadProps(options.osaamisenhankkimistapa)
           },
           "osaamisen-hankkimistapa-koodi-versio": {
+            "ui:widget": "hidden"
+          },
+          "oppisopimuksen-perusta-uri": {
+            "ui:field": "typeahead",
+            typeahead: typeaheadProps(options.oppisopimuksenperusta)
+          },
+          "oppisopimuksen-perusta-versio": {
             "ui:widget": "hidden"
           },
           "hankkijan-edustaja": {
