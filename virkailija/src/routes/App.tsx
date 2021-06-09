58214697bae3ffd36a130d75f810d5e4b9f5d305
samje <sami.honkanen@cybercom.com>
2021-06-09 16:56:12 +0300

    OY-2712-fix: remove unneeded virkailijaraamit component

--- virkailija/src/routes/App.tsx
+++ virkailija/src/routes/App.tsx
@@ -79,7 +79,6 @@ export class App extends React.Component<AppProps> {
       return (
         <ThemeWrapper>
           <Container>
-            <VirkailijaRaamit />
             <br />
             <LoadingSpinner />
             <GlobalStyles />
