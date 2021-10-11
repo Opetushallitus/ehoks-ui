6c2dfc239354793614518abb0392aea26a75526c
samje <sami.honkanen@cybercom.com>
2021-10-11 14:44:06 +0300

    Move PoistaHoks to separate file

--- shared/components/Opiskelija/Tavoitteet.tsx
+++ shared/components/Opiskelija/Tavoitteet.tsx
@@ -1,24 +1,22 @@
-import { Link, RouteComponentProps } from "@reach/router"
+import { RouteComponentProps } from "@reach/router"
 import { Accordion } from "components/Accordion"
 import { AccordionTitle } from "components/AccordionTitle"
 import { AppContext } from "components/AppContext"
-import { Button } from "components/Button"
 import { FormattedDate } from "components/FormattedDate"
 import { HeadingContainer, HelpHeading } from "components/Heading"
 import { HelpPopup } from "components/HelpPopup"
 import { InfoTable } from "components/InfoTable"
 import { LabeledColumn } from "components/LabeledColumn"
-import { ModalDialog } from "components/ModalDialogs/ModalDialog"
 import { inject, observer } from "mobx-react"
 import { IHOKS } from "models/HOKS"
 import { ISessionUser } from "models/SessionUser"
 import React from "react"
 import { FormattedMessage } from "react-intl"
-import styled from "../../../shared/styled"
 import { Opiskelijapalaute } from "../../../virkailija/src/routes/KoulutuksenJarjestaja/Opiskelijapalaute"
 import { IKoodistoVastaus } from "../../models/KoodistoVastaus"
 import { StudyPoints } from "../StudyPoints"
 import { IRootStore } from "../../../virkailija/src/stores/RootStore"
+import { HoksPoisto } from "./HoksPoisto"
 
 interface OsaamisenHankkimisenTarveProps {
   osaamisenHankkimisenTarve: boolean | null
@@ -225,170 +223,6 @@ const Urasuunnitelma = ({
     )}
   </Accordion>
 )
-
-const DeleteMessageContainer = styled("div")`
-  border-style: dashed;
-  border-color: gray;
-  border-radius: 5px;
-  padding: 10px 30px;
-  display: inline-flex;
-`
-
-const ButtonContainer = styled("div")`
-  display: flex;
-  align-items: left;
-  justify-content: left;
-  margin: 20px 0 10px 0;
-`
-
-const DeleteHoksButton = styled(Button)`
-  background: salmon;
-  color: white;
-  padding: 10px 30px;
-  font-size: 16px;
-
-  &:focus {
-    outline: none;
-    box-shadow: none;
-  }
-`
-const HoksInfoRow = styled("div")`
-  clear: both;
-`
-
-const HoksInfoTitleCell = styled("div")`
-  width: 45%;
-  display: inline-block;
-  padding: 10px 0px;
-  vertical-align: top;
-  font-weight: 400;
-  color: #6e6e7e;
-  text-align: left;
-`
-
-const HoksInfoValueCell = styled("div")`
-  width: 45%;
-  display: inline-block;
-  padding: 10px 0px;
-  vertical-align: top;
-`
-
-const DeleteLink = styled(Link)`
-  margin-right: 2em;
-`
-const CancelButton = styled(Button)`
-  background: transparent;
-  color: #000;
-`
-
-const HOKSpoisto = ({
-  title,
-  hoks,
-  student,
-  hoksPoistoOpen,
-  toggleHoksPoisto,
-  hoksPoistoModalOpen,
-  closeHoksPoistoModal,
-  openHoksPoistoModal,
-  shallowDeleteHoks
-}: {
-  title: React.ReactNode
-  hoks: IHOKS
-  student: ISessionUser
-  hoksPoistoOpen: boolean
-  hoksPoistoModalOpen: boolean
-  toggleHoksPoisto: () => void
-  closeHoksPoistoModal: () => void
-  openHoksPoistoModal: () => void
-  shallowDeleteHoks: (hoks: IHOKS) => void
-}) => (
-  <Accordion
-    id="hoksPoisto"
-    open={hoksPoistoOpen}
-    title={title}
-    onToggle={toggleHoksPoisto}
-  >
-    <DeleteMessageContainer>
-      <FormattedMessage
-        id="tavoitteet.PoistaHoksInfo"
-        defaultMessage="placeholder"
-      />
-    </DeleteMessageContainer>
-    <br />
-    <ButtonContainer>
-      <DeleteHoksButton onClick={openHoksPoistoModal}>
-        Poista HOKS
-      </DeleteHoksButton>
-    </ButtonContainer>
-
-    <ModalDialog
-      open={hoksPoistoModalOpen}
-      closeModal={closeHoksPoistoModal}
-      label="Haluatko varmasti poistaa hoksin?"
-    >
-      <b>
-        <FormattedMessage
-          id="tavoitteet.PoistaHoksConfirm"
-          defaultMessage="Haluatko varmasti poistaa hoksin?"
-        />
-      </b>
-      <br />
-      <br />
-      <HoksInfoRow>
-        <HoksInfoTitleCell>
-          <FormattedMessage id="tavoitteet.idTitle" defaultMessage="eHoks ID" />
-        </HoksInfoTitleCell>
-        <HoksInfoValueCell>{hoks.id}</HoksInfoValueCell>
-      </HoksInfoRow>
-      <HoksInfoRow>
-        <HoksInfoTitleCell>
-          <FormattedMessage
-            id="koulutuksenJarjestaja.opiskelijaTitle"
-            defaultMessage="Opiskelijan nimi"
-          />
-        </HoksInfoTitleCell>
-        <HoksInfoValueCell>{student.fullName}</HoksInfoValueCell>
-      </HoksInfoRow>
-      <HoksInfoRow>
-        <HoksInfoTitleCell>
-          <FormattedMessage
-            id="opiskelusuunnitelma.tutkinnonNimiTitle"
-            defaultMessage="Tutkinnon nimi"
-          />
-        </HoksInfoTitleCell>
-        <HoksInfoValueCell>{hoks.tutkinnonNimi}</HoksInfoValueCell>
-      </HoksInfoRow>
-      <HoksInfoRow>
-        <HoksInfoTitleCell>
-          <FormattedMessage
-            id="tavoitteet.opiskeluoikeudOidTitle"
-            defaultMessage="Opiskeluoikeus-oid"
-          />
-        </HoksInfoTitleCell>
-        <HoksInfoValueCell>{hoks.opiskeluoikeusOid}</HoksInfoValueCell>
-      </HoksInfoRow>
-      <HoksInfoRow>
-        <HoksInfoTitleCell>
-          <FormattedMessage
-            id="tavoitteet.OppilaitoksenNimi"
-            defaultMessage="Oppilaitoksen nimi"
-          />
-        </HoksInfoTitleCell>
-        <HoksInfoValueCell>{hoks.oppilaitos}</HoksInfoValueCell>
-      </HoksInfoRow>
-      <ButtonContainer>
-        <DeleteLink
-          to={`/ehoks-virkailija-ui/koulutuksenjarjestaja`}
-          onClick={() => shallowDeleteHoks(hoks)}
-        >
-          <DeleteHoksButton>Poista</DeleteHoksButton>
-        </DeleteLink>
-        <CancelButton onClick={closeHoksPoistoModal}>Peruuta</CancelButton>
-      </ButtonContainer>
-    </ModalDialog>
-  </Accordion>
-)
-
 const TutkintoTaiKoulutus = ({
   title,
   tutkintoTaiKoulutusOpen,
@@ -642,28 +476,6 @@ export class Tavoitteet extends React.Component<
     }))
   }
 
-  closeHoksPoistoModal = () => {
-    this.setState(state => ({
-      ...state,
-      hoksPoistoModalOpen: false
-    }))
-  }
-
-  openHoksPoistoModal = () => {
-    this.setState(state => ({
-      ...state,
-      hoksPoistoModalOpen: true
-    }))
-  }
-
-  shallowDeleteHoks = async (hoks: IHOKS) => {
-    hoks.shallowDelete()
-    this.setState(state => ({
-      ...state,
-      hoksPoistoModalOpen: false
-    }))
-  }
-
   render() {
     const { student, hoks, titles: customTitles = {} } = this.props
     const session = this.props.store!.session
@@ -782,16 +594,12 @@ export class Tavoitteet extends React.Component<
           />
         )}
         {session.hasShallowDeletePrivilege && app === "virkailija" && (
-          <HOKSpoisto
+          <HoksPoisto
             hoks={hoks}
             student={student}
             title={titles.hoksPoisto}
             hoksPoistoOpen={this.state.activeAccordions.hoksPoisto}
-            toggleHoksPoisto={this.toggleAccordion("hoksPoisto")}
-            hoksPoistoModalOpen={this.state.hoksPoistoModalOpen}
-            closeHoksPoistoModal={this.closeHoksPoistoModal}
-            openHoksPoistoModal={this.openHoksPoistoModal}
-            shallowDeleteHoks={this.shallowDeleteHoks}
+            toggleHoksPoisto={this.toggleAccordion}
           />
         )}
       </React.Fragment>
