bdbaf810ff9993838c5379a439dfa551c238d513
Kelvin Jackson <kelvin.jackson@gofore.com>
2021-10-20 09:59:34 +0300

    WIP: enemmän linter-ongelmia

--- virkailija/src/routes/MuokkaaHOKS.tsx
+++ virkailija/src/routes/MuokkaaHOKS.tsx
@@ -317,7 +317,7 @@ export class MuokkaaHOKS extends React.Component<
                   },
                   {
                     index: n,
-                    ohts: ohtErrors[ht][n as number].join(", ")
+                    ohts: ohtErrors[ht][parseInt(n)].join(", ")
                   }
                 )
               )
