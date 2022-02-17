165b99fc27fdf60c780ca591004a53abf9c33c5c
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-02-17 08:19:01 +0200

    WIP: Muokkaus uutta deployta varten

--- shared/components/InfoModal.tsx
+++ shared/components/InfoModal.tsx
@@ -137,7 +137,7 @@ export class InfoModal extends React.Component<InfoModalProps> {
                   <StyledStrong>
                     <FormattedMessage
                       id="infoModal.hankkimistapaTyyppi"
-                      defaultMessage="Osaamisen hankkimistapatyyppi"
+                      defaultMessage="Osaamisen hankkimistavan tyyppi"
                     />
                     :
                   </StyledStrong>
