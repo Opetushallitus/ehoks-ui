2a64152e5af67b8ab779f8ed3849bde2765b70d9
samje <sami.honkanen@cybercom.com>
2022-06-10 16:43:53 +0300

    WIP

--- shared/components/RaportitTable.tsx
+++ shared/components/RaportitTable.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React, { useMemo } from "react"
+import React, { useMemo, useEffect } from "react"
 import { Column, useTable, usePagination } from "react-table"
 // @ts-ignore Ignore type-checking for this library
 import TableScrollbar from "react-table-scrollbar"
@@ -7,6 +7,9 @@ import TableScrollbar from "react-table-scrollbar"
 interface RaportitTableProps {
   data: any
   columns: Column[]
+  loading: boolean
+  pageCount: number
+  fetchData?: (pageSize: number, pageIndex: number) => void
 }
 
 export function RaportitTable(props: RaportitTableProps) {
@@ -29,78 +32,105 @@ export function RaportitTable(props: RaportitTableProps) {
     previousPage,
     setPageSize,
     state: { pageIndex, pageSize }
-  } = useTable({ columns, data }, usePagination)
+  } = useTable(
+    { columns, data, manualPagination: true, pageCount: props.pageCount },
+    usePagination
+  )
+
+  useEffect(() => {
+    fetchData({ pageIndex, pageSize })
+  }, [pageIndex, pageSize])
+
   /* eslint-disable react/jsx-key */
   /* the jsx key is provided in the .get*Props() spreads. */
   return (
-    <TableScrollbar rows={20}>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map(row => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => (
-                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+    <>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+      <TableScrollbar rows={30}>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
                 ))}
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
-      <div className="pagination">
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {"<<"}
-        </button>{" "}
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {"<"}
-        </button>{" "}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {">"}
-        </button>{" "}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {">>"}
-        </button>{" "}
-        <span>
-          Page{" "}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>{" "}
-        </span>
-        <span>
-          | Go to page:{" "}
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={e => {
-              const p = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(p)
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  ))}
+                </tr>
+              )
+            })}
+            <tr>
+              {props.loading ? (
+                <td colSpan="10000">Loading...</td>
+              ) : (
+                <td colSpan="10000">
+                  Showing {page.length} of ~{props.pageCount * pageSize} results
+                </td>
+              )}
+            </tr>
+          </tbody>
+        </table>
+        <div className="pagination">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {"<<"}
+          </button>{" "}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {"<"}
+          </button>{" "}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {">"}
+          </button>{" "}
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>{" "}
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <select
+            value={pageSize}
+            onBlur={e => {
+              setPageSize(Number(e.target.value))
             }}
-            style={{ width: "100px" }}
-          />
-        </span>{" "}
-        <select
-          value={pageSize}
-          onBlur={e => {
-            setPageSize(Number(e.target.value))
-          }}
-        >
-          {[10, 20, 30, 40, 50].map(pSize => (
-            <option key={pSize} value={pSize}>
-              Show {pSize}
-            </option>
-          ))}
-        </select>
-      </div>
-    </TableScrollbar>
+          >
+            {[10, 20, 30, 40, 50].map(pSize => (
+              <option key={pSize} value={pSize}>
+                Show {pSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      </TableScrollbar>
+    </>
   )
 }
