9f73768893c7feb2e63c56e52602c4cf6cc41bbb
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-04-05 15:06:25 +0300

    Return otsikko as element

--- shared/models/helpers/getOtsikko.ts
+++
@@ -1,7 +0,0 @@
-export const getOtsikko = (
-  model: { otsikko: JSX.Element | string; osaamispisteet: number },
-  ospLyhenne: string
-): JSX.Element | string =>
-  `${model.otsikko}${
-    model.osaamispisteet ? ` ${model.osaamispisteet} ${ospLyhenne}` : ""
-  }`
