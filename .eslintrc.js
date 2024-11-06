acdd7af2b823a01aa06e93e6527074df25947424
Tomi Katajanheimo <tomi@katajanheimo.com>
2024-11-06 10:11:30 +0200

    EH-1715 rollback .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:react-hooks/recommended",
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "plugin:jsx-a11y/recommended"
   ],
@@ -30,6 +29,7 @@ module.exports = {
     }
   },
   rules: {
+    "semi": "off",
     "no-extra-boolean-cast": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
