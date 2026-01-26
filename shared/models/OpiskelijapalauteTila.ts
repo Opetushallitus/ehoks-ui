fc0b9ae9a3c8ba2cae5b085952460fad5affadb2
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2026-01-26 14:41:02 +0200

    EH-1991: don't require OpiskelijapalauteTila fields to exist

--- shared/models/OpiskelijapalauteTila.ts
+++ shared/models/OpiskelijapalauteTila.ts
@@ -2,13 +2,13 @@ import { types, Instance } from "mobx-state-tree"
 
 export const OpiskelijapalauteTila = types.model("OpiskelijapalauteTila", {
   hoksId: types.integer,
-  oppijaOid: types.string,
-  alkupvm: types.string,
-  lahetyspvm: types.string,
-  sahkoposti: types.string,
-  tyyppi: types.string,
-  voimassaLoppupvm: types.string,
-  lahetystila: types.string
+  oppijaOid: types.optional(types.string, ""),
+  alkupvm: types.optional(types.string, ""),
+  lahetyspvm: types.optional(types.string, ""),
+  sahkoposti: types.optional(types.string, ""),
+  tyyppi: types.optional(types.string, ""),
+  voimassaLoppupvm: types.optional(types.string, ""),
+  lahetystila: types.optional(types.string, "")
 })
 
 export type IOpiskelijapalauteTila = Instance<typeof OpiskelijapalauteTila>
